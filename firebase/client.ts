c63f1688fe47ba16601bc0b7c1cc14015e8e7e6f
AdityaAryan-1408 <aryanaditya1486@gmail.com>
2025-10-27 03:05:23 +0530

    Implemented Auth and Storage

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBdws8KjeaiLeBT1ZpBXepoYaEWvo3hUpA",
-    authDomain: "prepbuddy-4418b.firebaseapp.com",
-    projectId: "prepbuddy-4418b",
-    storageBucket: "prepbuddy-4418b.firebasestorage.app",
-    messagingSenderId: "85429379109",
-    appId: "1:85429379109:web:d1e3187ce391f4d8531fcc",
-    measurementId: "G-09Y03NL58Q"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
